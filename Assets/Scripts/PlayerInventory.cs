c4d510e6559677f3766214564bd2861f3c963c95
frederic0524 <onepiec0524@gmail.com>
2024-11-06 10:58:09 +0900

    [Feat] 간단한 아이템 수집 및 인벤토리 제작

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public int crystalCount = 0;
+    public int plantCount = 0;
+    public int bushCount = 0;
+    public int treeCount = 0;
+    // Start is called before the first frame update
+    private void AddItem(ItemType itemType)
+    {
+        switch (itemType)
+        {
+            case ItemType.Crystal:
+                crystalCount++;
+                Debug.Log($"크리스탈 획득! 현재 개수 :{crystalCount} ");
+                break;
+            case ItemType.Plant:
+                crystalCount++;
+                Debug.Log($"식물 획득! 현재 개수 :{plantCount} ");
+                break;
+            case ItemType.Bush:
+                crystalCount++;
+                Debug.Log($"수풀 획득! 현재 개수 :{bushCount} ");
+                break;
+            case ItemType.Tree:
+                crystalCount++;
+                Debug.Log($"나무 획득! 현재 개수 :{treeCount} ");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ShowInventory();
+        }
+
+    }
+
+
+
+
+    private void ShowInventory()
+    {
+        Debug.Log("====인벤토리====");
+        Debug.Log($"크리스탈:{crystalCount}개");
+        Debug.Log($"식물:{plantCount}개");
+        Debug.Log($"수풀:{bushCount}개");
+        Debug.Log($"나무:{treeCount}개");
+        Debug.Log("=================");
+    }
+}
